bfe82f53f29cce9a5df0fca89944b9bb5c7c350e
kykie-hub <kykie1008@gmail.com>
2026-06-17 23:23:27 +0800

    H1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿while (true)
+{
+    Console.WriteLine("1. Add Student");
+    Console.WriteLine("2. View Students");
+    Console.WriteLine("3. Statistics");
+    Console.WriteLine("4. Exit");
+
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            // Add Student
+            break;
+
+        case 2:
+            // View Students
+            break;
+
+        case 3:
+            // Statistics
+            break;
+
+        case 4:
+            return;
+    }
+}
